2a57ba51877c18bb17c962012279e3aea53725cf
Kenneth Vo <kennyvo91@gmail.com>
2017-05-08 22:57:46 -0500

    cleaned up JS

--- js/app.js
+++ js/app.js
@@ -1,71 +1,88 @@
-// wait for the DOM to finish loading
+
+var moveCount = 0;
+var player = 'player 1';
+
 $(document).ready(function() {
 
   console.log('Sanity Check. You are Sane.');
-  var totalMoves = 0
-  var win = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
-
-  //Switching players
-  var currentPlayer = "playerOne";
-  var firstPlayer = "playerOne";
-  var secondPlayer = "playerTwo";
-
-  function switchPlayer() {
-    if (currentPlayer === "playerOne"){
-      currentPlayer = secondPlayer;
-    } else {
-      currentPlayer = firstPlayer;
-    }
-  }
-  // click function
-  $('.col-xs-4').click(function() {
-    console.log(currentPlayer);
-    //box is claimed
+// alternate players on click
+  $('.col-xs-4').click(function(){
+    // blocks occupied squares
     if ( $(this).hasClass('playerOne') || $(this).hasClass('playerTwo')){
-      alert("Nope");
-    } else {
-      // update box and totalMoves var
-      $(this).addClass(currentPlayer);
-      switchPlayer();
-      totalMoves++;
-    }
-
-    console.log(totalMoves);
-    if (totalMoves > 9) {
-      alert("Game!");
-    }
-
+      $('#info').text("Nope. Taken.");
+    } else if  (moveCount % 2 == 0) {x
+        $(this).addClass('playerOne');
+        $('#info').text("Player 1. Your move.");
+        player = 'player 2';
+        moveCount++;
+      } else {
+        $(this).addClass('playerTwo');
+        $('#info').text("Player 2. Your move.");
+        player = 'player 1';
+        moveCount++;
+      }
+      // check winners
+      checkWin();
   });
 
-  // reset
+  // win conditions
+  function checkWin (){
+    if ( $('#1').hasClass('playerOne') &&  $('#2').hasClass('playerOne') && $('#3').hasClass('playerOne') ){
+      $('#1').addClass('win') && $('#2').addClass('win') && $('#3').addClass('win');
+      $('#info').text("Player 1 is the winner!!");
+    } else if ( $('#4').hasClass('playerOne') &&  $('#5').hasClass('playerOne') && $('#6').hasClass('playerOne') ){
+        $('#4').addClass('win') && $('#5').addClass('win') && $('#6').addClass('win');
+        $('#info').text("Player 1 is the winner!!");
+    } else if ( $('#7').hasClass('playerOne') &&  $('#8').hasClass('playerOne') && $('#9').hasClass('playerOne') ){
+        $('#7').addClass('win') && $('#8').addClass('win') && $('#9').addClass('win');
+        $('#info').text("Player 1 is the winner!!");
+    } else if ( $('#1').hasClass('playerOne') &&  $('#4').hasClass('playerOne') && $('#7').hasClass('playerOne') ){
+        $('#1').addClass('win') && $('#4').addClass('win') && $('#7').addClass('win');
+        $('#info').text("Player 1 is the winner!!");
+    } else if ( $('#2').hasClass('playerOne') &&  $('#5').hasClass('playerOne') && $('#8').hasClass('playerOne') ){
+        $('#2').addClass('win') && $('#5').addClass('win') && $('#8').addClass('win');
+        $('#info').text("Player 1 is the winner!!");
+    } else if ( $('#3').hasClass('playerOne') &&  $('#6').hasClass('playerOne') && $('#9').hasClass('playerOne') ){
+        $('#3').addClass('win') && $('#6').addClass('win') && $('#9').addClass('win');
+        $('#info').text("Player 1 is the winner!!");
+    } else if ( $('#1').hasClass('playerOne') &&  $('#5').hasClass('playerOne') && $('#9').hasClass('playerOne') ){
+        $('#1').addClass('win') && $('#5').addClass('win') && $('#9').addClass('win');
+        $('#info').text("Player 1 is the winner!!");
+    } else if ( $('#3').hasClass('playerOne') &&  $('#5').hasClass('playerOne') && $('#7').hasClass('playerOne') ){
+        $('#3').addClass('win') && $('#5').addClass('win') && $('#7').addClass('win');
+        $('#info').text("Player 1 is the winner!!");
+    } else if ( $('#4').hasClass('playerTwo') &&  $('#5').hasClass('playerTwo') && $('#6').hasClass('playerTwo') ){
+        $('#4').addClass('win') && $('#5').addClass('win') && $('#6').addClass('win');
+        $('#info').text("Player 2 is the winner!!");
+    } else if ( $('#7').hasClass('playerTwo') &&  $('#8').hasClass('playerTwo') && $('#9').hasClass('playerTwo') ){
+        $('#7').addClass('win') && $('#8').addClass('win') && $('#9').addClass('win');
+        $('#info').text("Player 2 is the winner!!");
+    } else if ( $('#1').hasClass('playerTwo') &&  $('#4').hasClass('playerTwo') && $('#7').hasClass('playerTwo') ){
+        $('#1').addClass('win') && $('#4').addClass('win') && $('#7').addClass('win');
+        $('#info').text("Player 2 is the winner!!");
+    } else if ( $('#2').hasClass('playerTwo') &&  $('#5').hasClass('playerTwo') && $('#8').hasClass('playerTwo') ){
+        $('#2').addClass('win') && $('#5').addClass('win') && $('#8').addClass('win');
+        $('#info').text("Player 2 is the winner!!");
+    } else if ( $('#3').hasClass('playerTwo') &&  $('#6').hasClass('playerTwo') && $('#9').hasClass('playerTwo') ){
+        $('#3').addClass('win') && $('#6').addClass('win') && $('#9').addClass('win');
+        $('#info').text("Player 2 is the winner!!");
+    } else if ( $('#1').hasClass('playerTwo') &&  $('#5').hasClass('playerTwo') && $('#9').hasClass('playerTwo') ){
+        $('#1').addClass('win') && $('#5').addClass('win') && $('#9').addClass('win');
+        $('#info').text("Player 2 is the winner!!");
+    } else if ( $('#3').hasClass('playerTwo') &&  $('#5').hasClass('playerTwo') && $('#7').hasClass('playerTwo') ){
+        $('#3').addClass('win') && $('#5').addClass('win') && $('#7').addClass('win');
+        $('#info').text("Player 2 is the winner!!");
+      // draw
+    } else if (moveCount === 9) {
+        $('#info').text("It's a draw. Fight to the death.");
+    }
+  };
+
+  // reset button
   $('.reset').click(function() {
-    $('.col-xs-4').removeClass("playerOne");
-    $('.col-xs-4').removeClass("playerTwo");
-    totalMoves = 0;
+    $('.col-xs-4').removeClass("playerOne playerTwo win");
+    $('#info').text("Player 1. Your move.");
+    moveCount = 0;
   });
-
 
 });
-
-
-// function checkWin (){
-//   var win = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
-//   if ( $('#1').hasClass('playerOne') &&  $('#2').hasClass('playerOne') && $('#3').hasClass('playerOne') ){
-//       alert( ' won!');
-//     }
-//   }
-// };
-
-// function checkWin (player){
-//   var win = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
-//   for (i=0; i<win.length; i++);
-//     if (playerOneArray[0] == win[0]){
-//       if (playerOneArray[1]) == win[1]){
-//         if (playerOneArray[2])== win[2]){
-//           alert('winner!');
-//         }
-//       }
-//     } else {
-//       break;
-//     }
-// };
--- js/app2.js
+++ js/app2.js
@@ -1,7 +1,10 @@
-// wait for the DOM to finish loading
 
 var moveCount = 0;
 var player = 'player 1';
+var playerOneArray = [];
+var playerTwoArray = [];
+var winningCombos = [['1','2','3'],['4','5','6'],['7','8','9'],['1','4','7'],['2','5','8'],['3','6','9'],['1','5','9'],['3','5','7']];
+var won = false;
 
 $(document).ready(function() {
 
@@ -12,67 +15,96 @@ $(document).ready(function() {
       $('#info').text("Nope. Taken.");
     } else if  (moveCount % 2 == 0) {
         $(this).addClass('playerOne');
+        playerOneArray.push($(this).attr('id'));
         $('#info').text("Player O. Your move.");
         player = 'player 2';
         moveCount++;
       } else {
         $(this).addClass('playerTwo');
+        playerTwoArray.push($(this).attr('id'));
         $('#info').text("Player X. Your move.");
         player = 'player 1';
         moveCount++;
       }
-      checkWin();
+      check();
   });
 
-  // win conditions
-  function checkWin (){
-    if ( $('#1').hasClass('playerOne') &&  $('#2').hasClass('playerOne') && $('#3').hasClass('playerOne') ){
-      $('#1').addClass('win') && $('#2').addClass('win') && $('#3').addClass('win');
-      $('#info').text("Player 1 is the winner!!");
-    } else if ( $('#4').hasClass('playerOne') &&  $('#5').hasClass('playerOne') && $('#6').hasClass('playerOne') ){
-        $('#4').addClass('win') && $('#5').addClass('win') && $('#6').addClass('win');
+  //    function checkWinTwo() {
+  //   //   var winningCombos = [['1','2','3'],['4','5','6'],['7','8','9'],['1','4','7'],['2','5','8'],['3','6','9'],['1','5','9'],['3','5','7']];
+    //   if ( moveCount > 4) {
+    //     playerOneArray.sort();
+    //     playerTwoArray.sort();
+  // win.forEach(function(checkCombo){
+  //       if winningNumber > 0;
+  //         // win
+  //       }
+  //     }
+  //     function  (){
+  //       win.indexOf(playerOneArray[0]) *
+  //       playerOneArray.indexOf(playerOneArray[1]) *
+  //       playerOneArray.indexOf(playerOneArray[2]) *
+  //       playerOneArray.indexOf(playerOneArray[4]) *
+  //       playerOneArray.indexOf(playerOneArray[5])
+  //     }
+  // }
+  // function checkWinThree() {
+  //   var winningCombos = [['1','2','3'],['4','5','6'],['7','8','9'],['1','4','7'],['2','5','8'],['3','6','9'],['1','5','9'],['3','5','7']];
+  //   if ( moveCount > 4) {
+  //     playerOneArray.sort();
+  //     playerTwoArray.sort();
+  //     for (i=0; i < winningCombos.length; i++) {
+  //       for (var j=0; j < playerOneArray.length; j++) {
+  //         console.log(i);
+  //         if (playerOneArray[j] === winningCombos[i][0]){
+  //             console.log(playerOneArray[j] === winningCombos[i][0])
+            // if (playerOneArray[j] === win[i][1]) {
+            //   if (playerOneArray[j] === win[i][2]){
+                // $('#info').text("Player 1 is the winner!!");
+            //   }
+  //           // }
+  //         }
+  //       }
+  //     }
+  //   }
+  // }
+
+  // function threeInARow(elem, index, arr){
+  //   for (var i = 0; i < playerOneArray.length; i++) {
+  //     if (playerOneArray[i] === winningCombos[i]
+  //   }
+  // }
+  //
+  // function checkWinFour () {
+  //   var winningCombos = [['1','2','3'],['4','5','6'],['7','8','9'],['1','4','7'],['2','5','8'],['3','6','9'],['1','5','9'],['3','5','7']];
+  //   winningCombos.forEach(function(threeInARow){
+  //       if (true) {
+  //         $('#info').text("Player 1 is the winner!!");
+  //       }
+  //   }
+  // }
+
+
+
+
+  function includesAll() {
+    for (var i = 0; i< arguments.length; i++) {
+      if (!this.includes(arguments[i])) {return false; }
+    }
+    return true
+  };
+
+    check = function() {
+    winningCombos.forEach(function(el, i, arr){
+      if (includesAll.apply(playerOneArray, el)) {
         $('#info').text("Player 1 is the winner!!");
-    } else if ( $('#7').hasClass('playerOne') &&  $('#8').hasClass('playerOne') && $('#9').hasClass('playerOne') ){
-        $('#7').addClass('win') && $('#8').addClass('win') && $('#9').addClass('win');
-        $('#info').text("Player 1 is the winner!!");
-    } else if ( $('#1').hasClass('playerOne') &&  $('#4').hasClass('playerOne') && $('#7').hasClass('playerOne') ){
-        $('#1').addClass('win') && $('#4').addClass('win') && $('#7').addClass('win');
-        $('#info').text("Player 1 is the winner!!");
-    } else if ( $('#2').hasClass('playerOne') &&  $('#5').hasClass('playerOne') && $('#8').hasClass('playerOne') ){
-        $('#2').addClass('win') && $('#5').addClass('win') && $('#8').addClass('win');
-        $('#info').text("Player 1 is the winner!!");
-    } else if ( $('#3').hasClass('playerOne') &&  $('#6').hasClass('playerOne') && $('#9').hasClass('playerOne') ){
-        $('#3').addClass('win') && $('#6').addClass('win') && $('#9').addClass('win');
-        $('#info').text("Player 1 is the winner!!");
-    } else if ( $('#1').hasClass('playerOne') &&  $('#5').hasClass('playerOne') && $('#9').hasClass('playerOne') ){
-        $('#1').addClass('win') && $('#5').addClass('win') && $('#9').addClass('win');
-        $('#info').text("Player 1 is the winner!!");
-    } else if ( $('#3').hasClass('playerOne') &&  $('#5').hasClass('playerOne') && $('#7').hasClass('playerOne') ){
-        $('#3').addClass('win') && $('#5').addClass('win') && $('#7').addClass('win');
-        $('#info').text("Player 1 is the winner!!");
-    } else if ( $('#4').hasClass('playerTwo') &&  $('#5').hasClass('playerTwo') && $('#6').hasClass('playerTwo') ){
-        $('#4').addClass('win') && $('#5').addClass('win') && $('#6').addClass('win');
-        $('#info').text("Player 2 is the winner!!");
-    } else if ( $('#7').hasClass('playerTwo') &&  $('#8').hasClass('playerTwo') && $('#9').hasClass('playerTwo') ){
-        $('#7').addClass('win') && $('#8').addClass('win') && $('#9').addClass('win');
-        $('#info').text("Player 2 is the winner!!");
-    } else if ( $('#1').hasClass('playerTwo') &&  $('#4').hasClass('playerTwo') && $('#7').hasClass('playerTwo') ){
-        $('#1').addClass('win') && $('#4').addClass('win') && $('#7').addClass('win');
-        $('#info').text("Player 2 is the winner!!");
-    } else if ( $('#2').hasClass('playerTwo') &&  $('#5').hasClass('playerTwo') && $('#8').hasClass('playerTwo') ){
-        $('#2').addClass('win') && $('#5').addClass('win') && $('#8').addClass('win');
-        $('#info').text("Player 2 is the winner!!");
-    } else if ( $('#3').hasClass('playerTwo') &&  $('#6').hasClass('playerTwo') && $('#9').hasClass('playerTwo') ){
-        $('#3').addClass('win') && $('#6').addClass('win') && $('#9').addClass('win');
-        $('#info').text("Player 2 is the winner!!");
-    } else if ( $('#1').hasClass('playerTwo') &&  $('#5').hasClass('playerTwo') && $('#9').hasClass('playerTwo') ){
-        $('#1').addClass('win') && $('#5').addClass('win') && $('#9').addClass('win');
-        $('#info').text("Player 2 is the winner!!");
-    } else if ( $('#3').hasClass('playerTwo') &&  $('#5').hasClass('playerTwo') && $('#7').hasClass('playerTwo') ){
-        $('#3').addClass('win') && $('#5').addClass('win') && $('#7').addClass('win');
-        $('#info').text("Player 2 is the winner!!");
-    } else if (moveCount === 9) {
-        $('#info').text("It's a draw. Fight to the death.");
+        won = true;
+      } else if (includesAll.apply(playerTwoArray, el)){
+          $('#info').text("Player 2 is the winner!!");
+          won = true;
+      }
+    })
+    if (playerOneArray.length === 5 && playerTwoArray.length === 4 && !won) {
+      $('#info').text("It's a draw. Fight to the death.");
     }
   };
 
@@ -81,6 +113,9 @@ $(document).ready(function() {
     $('.col-xs-4').removeClass("playerOne playerTwo win");
     $('#info').text("Player X. Your move.");
     moveCount = 0;
+    playerOneArray.length = 0;
+    playerTwoArray.length = 0;
+    var won = false;
   });
 
 });
